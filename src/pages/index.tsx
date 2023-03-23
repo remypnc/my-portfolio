4eba07ffea84ae5a56bc05e857973c621025fe72
Evan QUINCHON <quinch_e@etna-alternance.io>
2023-03-23 18:13:02 +0100

    entete end + install sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import Navbar from '../../component/navbar'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Entete from '../../component/entete'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +16,8 @@ export default function Home() {
       </Head>
       
       <main className={styles.main}>
-        <Navbar/>
+        <Navbar />
+        <Entete />
       </main>
     </>
   )
