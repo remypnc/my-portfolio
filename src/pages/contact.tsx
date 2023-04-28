2f431da8e43f741d11d68a5b9a629cc3bd4dbddc
Evan QUINCHON <quinch_e@etna-alternance.io>
2023-04-28 14:05:31 +0200

    contacrt push

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -20,6 +20,23 @@ export default function Home() {
                   <img src="../photo.jpg" />
                   <a href="#"> @remy_pnc</a>
                 </li>
+                <li>
+                  <img src="../photo.jpg" />
+                  <a href="#"> Rémy PENICHON</a>
+                </li>
+                <li>
+                  <img src="../photo.jpg" />
+                  <a href="tel:0614460516"> 06 14 46 05 16</a>
+                </li>
+                <li>
+                  <img src="../photo.jpg" />
+                  <div>
+                    <a href="mailto:penich_r@etna-alternance.net"> penich_r@etna-alternance.net</a>
+                    <a className={styles.gray} href="mailto:rpenichon18@gmail.com">
+                      ( rpenichon18@gmail.com )
+                    </a>
+                  </div>
+                </li>
               </ul>
             </div>
           </div>
