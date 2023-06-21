62deee4dd33d6ec711af6eccb521765600dcaf54
rpeni <penich_r@etna-alternance.net>
2023-06-21 15:42:16 +0200

    👌

--- component/entete.tsx
+++ component/entete.tsx
@@ -4,24 +4,22 @@ import styles from '@/styles/Entete.module.scss'
 const Entete = () => {
   return (
     <div className={styles.background}>
-      <div className={styles.yellow_bar}/>
+      <div className={styles.yellow_bar} />
       <section className={styles.square}>
         <div className={styles.container}>
-            <h3>Hi ! I’m Rémy,<br/> I’m a Web Developer !</h3>
-            <p>I’m a student in IT Technology and I’m passionate by Web Design.</p>
-            <div className={styles.space}>
-            <a href="/CV_Remy_PENICHON_Alternance_Web_Developper_Front-end.pdf" download>
-              <button>Download CV</button></a>
-                <button>Contact Me</button>
-            </div>
-            
+          <h3>Hi ! I’m Rémy,<br /> I’m a Web Designer & Developer !</h3>
+          <p>I’m a student in IT Technology and I’m passionate by Web Design.</p>
+          <div className={styles.space}>
+            <a href="/CV_Alternance_Web_Design_Front-end.pdf" download>
+              <button style={{ cursor: "pointer" }}>Download CV</button></a>
+            <button style={{ cursor: "pointer" }}>Contact Me</button>
+          </div>
         </div>
-        
       </section>
-      <div className={styles.yellow_bar}/>
+      <div className={styles.yellow_bar} />
     </div>
   );
 };
-    
+
 
 export default Entete;
